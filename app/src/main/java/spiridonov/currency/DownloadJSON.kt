56ba2a488aa28e846d937d2d1c058f3ef1c6f9ef
lenchikspiridonchik <2003.leonid2003@gmail.com>
2022-02-07 14:42:46 +0300

    shit

--- app/src/main/java/spiridonov/currency/DownloadJSON.kt
+++ app/src/main/java/spiridonov/currency/DownloadJSON.kt
@@ -1,3 +1,5 @@
+package spiridonov.currency
+
 import java.io.BufferedInputStream
 import java.io.BufferedReader
 import java.io.IOException
--- app/src/main/java/spiridonov/currency/MainActivity.kt
+++ app/src/main/java/spiridonov/currency/MainActivity.kt
@@ -1,11 +1,42 @@
 package spiridonov.currency
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.ProgressDialog
+import android.os.AsyncTask
 import android.os.Bundle
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    private val pDialog: ProgressDialog? = null
+    private lateinit var listView: ListView
+    private val url = "https://www.cbr-xml-daily.ru/daily_json.js"
+    lateinit var contactList: ArrayList<HashMap<String, String>>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        contactList = ArrayList()
+        listView = currencyList
+
+    }
+
+    private class GetCurrency : AsyncTask<Void?, Void?, Void?>(){
+        override fun onPreExecute() {
+            super.onPreExecute()
+            // Showing progress dialog
+        }
+
+        override fun doInBackground(vararg p0: Void?): Void? {
+            return null
+        }
+
+        override fun onPostExecute(result: Void?) {
+            super.onPostExecute(result)
+
+        }
     }
 }
+
