4c144cb77248e8dea411056376698754f906da25
lenchikspiridonchik <2003.leonid2003@gmail.com>
2022-02-07 21:03:52 +0300

    cool shit

--- app/src/main/java/spiridonov/currency/MainActivity.kt
+++ app/src/main/java/spiridonov/currency/MainActivity.kt
@@ -3,39 +3,71 @@ package spiridonov.currency
 import android.app.ProgressDialog
 import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONException
+import org.json.JSONObject
 
 
-class MainActivity : AppCompatActivity() {
-    private val pDialog: ProgressDialog? = null
+class MainActivity : AppCompatActivity() /*, CoroutineScope by MainScope()*/ {
+    private lateinit var pDialog: ProgressDialog
     private lateinit var listView: ListView
     private val url = "https://www.cbr-xml-daily.ru/daily_json.js"
-    lateinit var contactList: ArrayList<HashMap<String, String>>
+    lateinit var currList: ArrayList<HashMap<String, String>>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        contactList = ArrayList()
+        currList = ArrayList()
         listView = currencyList
+
+        GetCurrency().execute()
 
     }
 
-    private class GetCurrency : AsyncTask<Void?, Void?, Void?>(){
+
+    inner class GetCurrency : AsyncTask<Void?, Void?, Void?>() {
         override fun onPreExecute() {
             super.onPreExecute()
-            // Showing progress dialog
+            pDialog = ProgressDialog(this@MainActivity)
+            pDialog.setMessage("Базы обновляются...")
+            pDialog.show()
         }
 
         override fun doInBackground(vararg p0: Void?): Void? {
+            val downloadJSON = DownloadJSON()
+            val JSONString = downloadJSON.connection(url)
+            try {
+                val jsonObjects = JSONObject(JSONString)
+                val valute = jsonObjects.getJSONObject("Valute")
+                val allKeys = valute.keys()
+
+                for (i in 0 until valute.length()) {
+                    Log.d("Valute ID", valute.getString(allKeys.next()).toString())
+
+                }
+            } catch (e: JSONException) {
+                Log.e("JSONException", "Json parsing error: " + e.message.toString());
+                runOnUiThread {
+                    Toast.makeText(
+                        applicationContext,
+                        "Json parsing error: " + e.message.toString(),
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+            }
             return null
         }
 
         override fun onPostExecute(result: Void?) {
             super.onPostExecute(result)
-
+            if (pDialog.isShowing)
+                pDialog.dismiss();
         }
     }
 }
