f852d5e12f23042f772ba85a39bded7c94bc5462
lenchikspiridonchik <2003.leonid2003@gmail.com>
2022-02-08 14:32:53 +0300

    cool shit

--- app/src/main/java/spiridonov/currency/MainActivity.kt
+++ app/src/main/java/spiridonov/currency/MainActivity.kt
@@ -1,6 +1,8 @@
 package spiridonov.currency
 
 import android.app.ProgressDialog
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
@@ -15,24 +17,69 @@ import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     private lateinit var pDialog: ProgressDialog
-
+    private lateinit var msp: SharedPreferences
     private val url = "https://www.cbr-xml-daily.ru/daily_json.js"
-    lateinit var currList: ArrayList<HashMap<String, String>>
-    private var needToUpdate = true
+    private var needToUpdate = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        currList = ArrayList()
-
-        if (needToUpdate) GetCurrency().execute()
+        msp = getSharedPreferences("AppMemory", Context.MODE_PRIVATE)
+        var currString = ""
+        if (msp.contains(KEY_CURRENCY)) currString = msp.getString(KEY_CURRENCY, "").toString()
+        if (needToUpdate || currString.isEmpty()) GetCurrency().execute()
+        else parsingData(jsonString = currString)
 
 
     }
 
 
+    private fun parsingData(jsonString: String) {
+        val currList: ArrayList<HashMap<String, String>> = ArrayList()
+        try {
+            val jsonObjects = JSONObject(jsonString)
+            val valute = jsonObjects.getJSONObject("Valute")
+            val allKeys = valute.keys()
+
+            while (allKeys.hasNext()) {
+                val myObj = valute.getJSONObject(allKeys.next())
+                val currencyMap = HashMap<String, String>()
+                currencyMap["code"] = myObj.getString("CharCode")
+                currencyMap["name"] = myObj.getString("Name")
+                currencyMap["value"] = myObj.getString("Value")
+                currencyMap["previous"] = myObj.getString("Previous")
+                currList.add(currencyMap)
+            }
+            Log.d("parsingData", "parsingData")
+        } catch (e: JSONException) {
+            Log.e("JSONException", "Json parsing error: " + e.message.toString());
+            runOnUiThread {
+                Toast.makeText(
+                    applicationContext,
+                    "Json parsing error: " + e.message.toString(),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+        showCurrency(dataList = currList)
+    }
+
+
+    private fun showCurrency(dataList: ArrayList<HashMap<String, String>>) {
+        val adapter: ListAdapter = SimpleAdapter(
+            this@MainActivity,
+            dataList,
+            R.layout.each_currency,
+            arrayOf("code", "name", "value", "previous"),
+            intArrayOf(R.id.txtID, R.id.txtName, R.id.txtValue, R.id.txtPrevious)
+        )
+        currencyList.adapter = adapter
+    }
+
+
     inner class GetCurrency : AsyncTask<Void?, Void?, Void?>() {
+        private var jsonString = ""
         override fun onPreExecute() {
             super.onPreExecute()
             pDialog = ProgressDialog(this@MainActivity)
@@ -41,50 +88,24 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun doInBackground(vararg p0: Void?): Void? {
+            Log.d("doInBackground", "download currency")
             val downloadJSON = DownloadJSON()
-            val JSONString = downloadJSON.connection(url)
-            try {
-                val jsonObjects = JSONObject(JSONString)
-                val valute = jsonObjects.getJSONObject("Valute")
-                val allKeys = valute.keys()
-
-                while (allKeys.hasNext()) {
-                    val myObj = valute.getJSONObject(allKeys.next())
-                    val currencyMap = HashMap<String, String>()
-                    currencyMap["code"] = myObj.getString("CharCode")
-                    currencyMap["name"] = myObj.getString("Name")
-                    currencyMap["value"] = myObj.getString("Value")
-                    currencyMap["previous"] = myObj.getString("Previous")
-                    currList.add(currencyMap)
-                }
-
-            } catch (e: JSONException) {
-                Log.e("JSONException", "Json parsing error: " + e.message.toString());
-                runOnUiThread {
-                    Toast.makeText(
-                        applicationContext,
-                        "Json parsing error: " + e.message.toString(),
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-
-            }
+            jsonString = downloadJSON.connection(url)
             return null
         }
 
         override fun onPostExecute(result: Void?) {
             super.onPostExecute(result)
-            if (pDialog.isShowing)
-                pDialog.dismiss();
-            val adapter: ListAdapter = SimpleAdapter(
-                this@MainActivity,
-                currList,
-                R.layout.each_currency,
-                arrayOf("code", "name", "value", "previous"),
-                intArrayOf(R.id.txtID, R.id.txtName, R.id.txtValue, R.id.txtPrevious)
-            )
-            currencyList.adapter = adapter
+            parsingData(jsonString)
+            val editor = msp.edit()
+            editor.putString(KEY_CURRENCY, jsonString)
+            editor.apply()
+            if (pDialog.isShowing) pDialog.dismiss();
         }
+    }
+
+    companion object {
+        const val KEY_CURRENCY = "currency"
     }
 }
 
