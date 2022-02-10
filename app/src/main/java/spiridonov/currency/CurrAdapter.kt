4b5c346fc2301bad93ca5553337b641bd5d40b5f
lenchikspiridonchik <2003.leonid2003@gmail.com>
2022-02-10 22:15:08 +0300

    cool shit

--- app/src/main/java/spiridonov/currency/CurrAdapter.kt
+++ app/src/main/java/spiridonov/currency/CurrAdapter.kt
@@ -1,14 +1,17 @@
 package spiridonov.currency
 
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.each_currency.view.*
 
 
-class CurrAdapter(private var currList: ArrayList<CurrCard>) :
+class CurrAdapter(private var currList: ArrayList<CurrCard>, private val context: Context) :
     RecyclerView.Adapter<CurrAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -16,6 +19,7 @@ class CurrAdapter(private var currList: ArrayList<CurrCard>) :
         var txtCode: TextView = itemView.txtID
         var txtValue: TextView = itemView.txtValue
         var txtPrevious: TextView = itemView.txtPrevious
+        var imgStar: ImageView = itemView.imgStar
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(
@@ -28,7 +32,41 @@ class CurrAdapter(private var currList: ArrayList<CurrCard>) :
         holder.txtCode.text = currList[position].code
         holder.txtValue.text = currList[position].value
         holder.txtPrevious.text = currList[position].previous
+        holder.imgStar.setImageResource(R.drawable.star_no_rate)
+        val msp = context.getSharedPreferences("AppMemory", Context.MODE_PRIVATE)
+        var favouriteList = arrayListOf<String>()
+        if (msp.contains(KEY_FAVOURITE) && msp.getString(KEY_FAVOURITE, " , ") != "")
+            favouriteList = msp.getString(KEY_FAVOURITE, "")?.split(",") as ArrayList<String>
+        if (favouriteList.contains(currList[position].code)){
+            holder.imgStar.setImageResource(R.drawable.star_rate)
+        }
+
+        holder.imgStar.setOnClickListener {
+            if (msp.contains(KEY_FAVOURITE) && msp.getString(KEY_FAVOURITE, " , ") != "")
+                favouriteList = msp.getString(KEY_FAVOURITE, "")?.split(",") as ArrayList<String>
+
+            if (favouriteList.contains(currList[position].code)){
+                favouriteList.remove(currList[position].code)
+                holder.imgStar.setImageResource(R.drawable.star_no_rate)
+            }
+            else {
+                favouriteList.add(currList[position].code)
+                holder.imgStar.setImageResource(R.drawable.star_rate)
+            }
+            var buff = ""
+            favouriteList.forEach {
+                if (it != "") buff += "$it,"
+            }
+            Log.d("onBindViewHolder", "value $buff")
+            val editor = msp.edit()
+            editor.putString(KEY_FAVOURITE, buff)
+            editor.apply()
+        }
     }
 
     override fun getItemCount(): Int = currList.size
+
+    companion object {
+        const val KEY_FAVOURITE = "favourite"
+    }
 }
--- app/src/main/java/spiridonov/currency/MainActivity.kt
+++ app/src/main/java/spiridonov/currency/MainActivity.kt
@@ -28,12 +28,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nameValueList: HashMap<String, Double>
     private lateinit var nameCodeList: HashMap<String, String>
     private lateinit var listForSpiner: MutableList<String>
-    private lateinit var listnameValue: Array<Array<String?>>
+    private lateinit var arrayNameValue: Array<Array<String?>>
     private lateinit var lastUpdate: Calendar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        listnameValue = Array(2) { arrayOfNulls(2) }
+        arrayNameValue = Array(2) { arrayOfNulls(2) }
         msp = getSharedPreferences("AppMemory", Context.MODE_PRIVATE)
         nameValueList = hashMapOf()
         nameCodeList = hashMapOf()
@@ -50,8 +50,8 @@ class MainActivity : AppCompatActivity() {
 
         spinner1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                listnameValue[0][0] = listForSpiner[p2]
-                listnameValue[0][1] = nameValueList.getOrDefault(listForSpiner[p2], 1.0).toString()
+                arrayNameValue[0][0] = listForSpiner[p2]
+                arrayNameValue[0][1] = nameValueList.getOrDefault(listForSpiner[p2], 1.0).toString()
                 convertCurr()
             }
 
@@ -59,8 +59,8 @@ class MainActivity : AppCompatActivity() {
         }
         spinner2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                listnameValue[1][0] = listForSpiner[p2]
-                listnameValue[1][1] = nameValueList.getOrDefault(listForSpiner[p2], 1.0).toString()
+                arrayNameValue[1][0] = listForSpiner[p2]
+                arrayNameValue[1][1] = nameValueList.getOrDefault(listForSpiner[p2], 1.0).toString()
                 convertCurr()
             }
 
@@ -100,10 +100,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun convertCurr() {
         val enter = txtEnter.text.toString()
-        val firstNumb = listnameValue[0][1]?.toDouble()
-        val scndNumb = listnameValue[1][1]?.toDouble()
-        val firstValute = nameCodeList[listnameValue[0][0].toString()].toString()
-        val scndValute = nameCodeList[listnameValue[1][0].toString()].toString()
+        val firstNumb = arrayNameValue[0][1]?.toDouble()
+        val scndNumb = arrayNameValue[1][1]?.toDouble()
+        val firstValute = nameCodeList[arrayNameValue[0][0].toString()].toString()
+        val scndValute = nameCodeList[arrayNameValue[1][0].toString()].toString()
         if (firstNumb != null && scndNumb != null && firstNumb != 0.0 && scndNumb != 0.0 && enter.isNotEmpty() && enter.toDouble() > 0.0) {
             var enterSum = enter.toDouble()
             enterSum = (enterSum * firstNumb) / scndNumb
@@ -114,6 +114,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun parsingData(jsonString: String) {
         val currList: ArrayList<CurrCard> = ArrayList()
+        var currFavourite = arrayListOf<String>()
+        if (msp.contains(CurrAdapter.KEY_FAVOURITE) && msp.getString(CurrAdapter.KEY_FAVOURITE, " , ") != "")
+            currFavourite = msp.getString(CurrAdapter.KEY_FAVOURITE, " , ")?.split(",") as ArrayList<String>
         try {
             val jsonObjects = JSONObject(jsonString)
             lastUpdate = Calendar.getInstance()
@@ -121,23 +124,18 @@ class MainActivity : AppCompatActivity() {
                 SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(jsonObjects.getString("Date"))
             val valute = jsonObjects.getJSONObject("Valute")
             val allKeys = valute.keys()
-            while (allKeys.hasNext()) {
-                val myObj = valute.getJSONObject(allKeys.next())
-                val name = myObj.getString("Name")
-                val code = myObj.getString("CharCode")
-                var value = myObj.getDouble("Value")
-                val nominal = myObj.getDouble("Nominal")
-                value /= nominal
-                val currencyMap = CurrCard(
-                    code = code,
-                    name = name,
-                    value = String.format("%.4f", value),
-                    previous = myObj.getString("Previous")
-                )
-                currList.add(currencyMap)
-                nameValueList[name] = value
-                nameCodeList[name] = code
+
+            currFavourite.forEach {
+                if (it != "")
+                    currList.add(parsing(valute = valute, it = it))
             }
+
+            while (allKeys.hasNext()) {
+                val nextKey = allKeys.next()
+                if (currFavourite.contains(nextKey)) continue
+                currList.add(parsing(valute = valute, it = nextKey))
+            }
+
             nameCodeList["Российский рубль"] = "RUB"
         } catch (e: JSONException) {
             Log.e("JSONException", "Json parsing error: " + e.message.toString());
@@ -153,14 +151,33 @@ class MainActivity : AppCompatActivity() {
         listForSpiner.add("Российский рубль")
         val adapterCurrency: ArrayAdapter<String> =
             ArrayAdapter<String>(
-                this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, listForSpiner
+                this,
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                listForSpiner
             )
 
         adapterCurrency.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner1.adapter = adapterCurrency
         spinner2.adapter = adapterCurrency
         showCurrency(dataList = currList)
+    }
 
+    private fun parsing(valute: JSONObject, it: String): CurrCard {
+        val myObj = valute.getJSONObject(it)
+        val name = myObj.getString("Name")
+        val code = myObj.getString("CharCode")
+        var value = myObj.getDouble("Value")
+        val nominal = myObj.getDouble("Nominal")
+        value /= nominal
+        val currencyMap = CurrCard(
+            code = code,
+            name = name,
+            value = String.format("%.4f", value),
+            previous = myObj.getString("Previous")
+        )
+        nameValueList[name] = value
+        nameCodeList[name] = code
+        return currencyMap
     }
 
 
@@ -168,9 +185,8 @@ class MainActivity : AppCompatActivity() {
         recycleView.setHasFixedSize(true)
         val linearLayoutManager = LinearLayoutManager(this)
         recycleView.layoutManager = linearLayoutManager
-        val adapter = CurrAdapter(currList = dataList)
+        val adapter = CurrAdapter(currList = dataList, context = applicationContext)
         recycleView.adapter = adapter
-
     }
 
 
